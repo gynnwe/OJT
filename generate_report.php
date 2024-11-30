1516ab6d9ec45ab3f5cb2e8c07b3c62fdb226e11
banzavage <alisonbanzuelo@gmail.com>
2024-12-01 05:38:18 +0800

    fixed pdf format

--- generate_report.php
+++ generate_report.php
@@ -1,6 +1,14 @@
 <?php
 require_once 'vendor/autoload.php';
 
+// Custom TCPDF class to remove the default footer
+class CustomPDF extends TCPDF {
+    // Disable default footer
+    public function Footer() {
+        // Leave this function empty to suppress all footers
+    }
+}
+
 // Database connection
 $servername = "localhost";
 $username = "root";
@@ -56,18 +64,17 @@ if (!$logs) {
     die('No records found for this property number.');
 }
 
-// Create a new PDF document instance with A4 size
-$pdf = new TCPDF('P', PDF_UNIT, 'A4', true, 'UTF-8', false);
+// Create a new PDF document instance
+$pdf = new CustomPDF('P', PDF_UNIT, 'A4', true, 'UTF-8', false);
 $pdf->SetCreator(PDF_CREATOR);
 $pdf->SetAuthor('University of Southeastern Philippines');
 $pdf->SetTitle('ICT Equipment History Sheet');
 $pdf->SetSubject('Equipment Report');
 
 // Set margins
-$pdf->SetMargins(25.4, 25.4, 25.4); // 1 inch margins
-$pdf->SetHeaderMargin(10);
-$pdf->SetFooterMargin(10);
-$pdf->SetAutoPageBreak(TRUE, 25.4); // Bottom margin
+$pdf->SetMargins(10.9, 11.6, 10.9); // Left: 1.09 cm, Top: 1.16 cm, Right: 1.09 cm
+$pdf->SetHeaderMargin(12.7); // Header margin (1.27 cm)
+$pdf->SetAutoPageBreak(TRUE, 4.9); // Bottom margin (0.49 cm)
 
 // Add a new page
 $pdf->AddPage();
@@ -77,30 +84,47 @@ $logoPath = 'assets/usep-logo.jpg';
 
 // Header Content
 $html = '
-    <table border="0" cellpadding="5" cellspacing="0" width="100%">
-        <tr>
-            <td width="20%" align="center"><img src="' . $logoPath . '" width="70" height="70" /></td>
-            <td width="55%" align="center" style="font-size: 9px;">
-                <strong style="font-family: Arial;">Republic of the Philippines</strong><br>
-                <strong style="font-family: oldenglishttextmt; font-size: 16px;">University of Southeastern Philippines</strong><br>
-                <span style="font-family: Arial;">Iñigo St., Bo. Obrero, Davao City 8000</span><br>
-                <span style="font-family: Arial;">Telephone: (082) 227-8192</span><br>
-                <span style="font-family: Arial;">Website: <a href="http://www.usep.edu.ph">www.usep.edu.ph</a></span><br>
-                <span style="font-family: Arial;">Email: <a href="mailto:president@usep.edu.ph">president@usep.edu.ph</a></span>
-            </td>
-            <td width="25%" align="left">
-                <table border="0.5" cellpadding="2" cellspacing="0" style="font-size:7px; width: 100%; font-family: Arial;">
-                    <tr><td>Form No.</td><td>FM-USeP-ICT-04</td></tr>
-                    <tr><td>Issue Status</td><td>01</td></tr>
-                    <tr><td>Revision No.</td><td>00</td></tr>
-                    <tr><td>Date Effective</td><td>23 December 2022</td></tr>
-                    <tr><td>Approved by</td><td>President</td></tr>
-                </table>
-            </td>
-        </tr>
-    </table>
-    <hr>
-    <h2 align="center" style="font-size: 12px; font-family: Arial;">ICT EQUIPMENT HISTORY SHEET</h2>
+<table border="0.5" cellpadding="0.5" cellspacing="0" width="100%">
+    <tr>
+        <td width="20%" align="center"><img src="' . $logoPath . '" width="70" height="70" /></td>
+        <td width="50%" align="center">
+<span style="font-family: Arial; font-size: 9px;">Republic of the Philippines</span><br>
+            <strong style="font-family: oldenglishttextmt; font-size: 16px;">University of Southeastern Philippines</strong><br>
+            <span style="font-family: Arial; font-size: 9px; ">Iñigo St., Bo. Obrero, Davao City 8000</span><br>
+            <span style="font-family: Arial; font-size: 9px; ">Telephone: (082) 227-8192</span><br>
+            <span style="font-family: Arial; font-size: 9px; ">Website: <a href="http://www.usep.edu.ph">www.usep.edu.ph</a></span><br>
+            <span style="font-family: Arial; font-size: 9px; ">Email: <a href="mailto:president@usep.edu.ph">president@usep.edu.ph</a></span>
+        </td>
+        <td width="30%">
+<table border="0.5" cellpadding="4" cellspacing="0" style="font-size: 9px; width: 100%; font-family: Arial;">
+    <tr>
+        <td>Form No.</td>
+        <td>FM-USeP-ICT-04</td>
+    </tr>
+    <tr>
+        <td>Issue Status</td>
+        <td>01</td>
+    </tr>
+    <tr>
+        <td>Revision No.</td>
+        <td>00</td>
+    </tr>
+    <tr>
+        <td>Date Effective</td>
+        <td>23 December 2022</td>
+    </tr>
+    <tr>
+        <td>Approved by</td>
+        <td>President</td>
+    </tr>
+</table>
+
+
+        </td>
+    </tr>
+</table>
+<hr>
+<h2 align="center" style="font-size: 12px; font-family: Arial;">ICT EQUIPMENT HISTORY SHEET</h2>
 ';
 
 // Equipment Details Section (Calibri, 11pt)
@@ -152,9 +176,7 @@ $html .= '
         </tbody>
     </table>
     <br>
-    <div style="text-align: center; font-size: 10px;">
-        Systems and Data Management Division (SDMD) - Page 1 of 1
-    </div>';
+';
 
 // Write the HTML content to the PDF
 $pdf->writeHTML($html, true, false, true, false, '');
