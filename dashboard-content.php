d85ea4d2d1c53fef86fae4bf0f896377753b2316
Ciandryl Dayaganon <cbdayaganon@usep.edu.ph>
2024-12-30 09:34:54 +0800

    background update (2)

--- dashboard-content.php
+++ dashboard-content.php
@@ -62,7 +62,7 @@ try {
             box-shadow: 0 2px 4px rgba(0,0,0,0.1);
         }
         .solid-card {
-            background: white !important;
+            background: rgba(255, 255, 255, 0.6);
             border-radius: 8px;
             padding: 20px;
             box-shadow: 0 2px 4px rgba(0,0,0,0.1);
